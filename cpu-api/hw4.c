f121c6d69816ad18581e5f2478510d17a285ec45
Sy Rashid <sy.rashid.11@gmail.com>
2023-05-04 18:09:27 +0100

    FEAT: Add additional notes to hw4

--- cpu-api/hw4.c
+++ cpu-api/hw4.c
@@ -32,3 +32,21 @@ int main(int argc, char *argv[])
   }
   return 0;
 }
+
+// In general, the exec system calls are used to replace the current process image with a new process image. These functions are commonly used in conjunction with the fork system call, which creates a new process that is a copy of the current process. By calling one of the exec functions in the child process, you can replace the child process with a new program, while preserving any state that was inherited from the parent process.
+
+// The exec function is not actually a system call itself, but rather a family of functions that include execl, execle, execlp, execv, execvp, and execvpe. These functions all perform a similar task: replacing the current process image with a new process image.
+
+// The different versions of the exec system call in C serve slightly different purposes and are used in different contexts. Here's a brief overview of each of the six versions you mentioned:
+
+// execl : This function is used to execute a specified program with a list of arguments passed as separate parameters to the function.The final parameter must be a NULL pointer to terminate the list of arguments.
+
+// execle : This function is similar to execl,but it allows you to specify the environment of the new process as an additional parameter.The environment must be passed as an array of pointers to char, terminated by a NULL pointer.
+
+// execlp : This function is similar to execl, but it searches the system PATH environment variable to find the specified program.This allows you to execute a program without specifying its full path.
+
+// execv : This function is similar to execl, but it takes the arguments as an array of pointers to char, terminated by a NULL pointer.This can be more convenient than specifying each argument as a separate parameter, especially when you have a large number of arguments.
+
+// execvp : This function is similar to execv, but it searches the system PATH environment variable to find the specified program.
+
+// execvpe : This function is similar to execvp, but it allows you to specify the environment of the new process as an additional parameter.
