bab0f64ab1e374fdd8e8b951393f877138a288a9
Sy Rashid <sy.rashid.11@gmail.com>
2023-05-04 17:27:20 +0100

    FEAT: Set up hw problem with different print statements

--- cpu-api/hw1.c
+++ cpu-api/hw1.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdio.h>  // input and output
+#include <stdlib.h> // memory allocation
+#include <unistd.h> // process management
 
 int main(int argc, char *argv[])
 {
-  printf("hello world (pid:%d)\n", (int)getpid());
+  int x = 100;
+  printf("hello world (pid:%d) and the x value is %d\n", (int)getpid(), x);
   int rc = fork();
   if (rc < 0)
   {
@@ -15,13 +16,13 @@ int main(int argc, char *argv[])
   else if (rc == 0)
   {
     // child (new process)
-    printf("hello, I am child (pid:%d)\n", (int)getpid());
+    printf("hello, I am child (pid:%d) and the x value is %d\n", (int)getpid(), x);
   }
   else
   {
     // parent goes down this path (original process)
-    printf("hello, I am parent of %d (pid:%d)\n",
-           rc, (int)getpid());
+    printf("hello, I am parent of %d (pid:%d) and the x value is %d\n",
+           rc, (int)getpid(), x);
   }
   return 0;
 }
