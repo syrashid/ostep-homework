603039f4c25071187aec0b2d322b80f5852fccfd
Sy Rashid <sy.rashid.11@gmail.com>
2023-05-04 19:15:16 +0100

    FEAT: Finish hw7

---
+++ cpu-api/hw7.c
@@ -0,0 +1,32 @@
+#include <stdio.h>  // input and output
+#include <stdlib.h> // memory allocation
+#include <unistd.h> // process management
+
+int main(int argc, char *argv[])
+{
+
+  printf("hello world (pid:%d)\n", (int)getpid());
+  int rc = fork();
+  if (rc < 0)
+  {
+    // fork failed; exit
+    fprintf(stderr, "fork failed\n");
+    exit(1);
+  }
+  else if (rc == 0)
+  {
+    // child (new process)
+    printf("closing stdout\n");
+    close(1);
+    printf("hello, I am child (pid:%d)\n", (int)getpid());
+  }
+  else
+  {
+    // parent goes down this path (original process)
+    printf("hello, I am parent of %d (pid:%d)\n",
+           rc, (int)getpid());
+  }
+  return 0;
+}
+
+// Once the child closes stdout it no longer can print to the terminal
