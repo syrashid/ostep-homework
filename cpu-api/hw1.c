865a26d1ba71ee2e3e3ed8c6598d9cddd721e400
Sy Rashid <sy.rashid.11@gmail.com>
2023-05-04 17:31:38 +0100

    NOTE: Add some homework notes for hw1

--- cpu-api/hw1.c
+++ cpu-api/hw1.c
@@ -16,13 +16,21 @@ int main(int argc, char *argv[])
   else if (rc == 0)
   {
     // child (new process)
+    x = 200;
     printf("hello, I am child (pid:%d) and the x value is %d\n", (int)getpid(), x);
   }
   else
   {
     // parent goes down this path (original process)
+    x = 300;
     printf("hello, I am parent of %d (pid:%d) and the x value is %d\n",
            rc, (int)getpid(), x);
   }
   return 0;
 }
+
+// Key takeaway here with assigning the x value it seems that each process get's its own copy of the variable x
+
+// After the fork function is called, the child process creates a copy of the parent process's memory, including the value of x. Any changes made to x in the child process will only affect the child process's copy of x, and will not affect the parent process's copy of x.
+
+// Similarly, any changes made to x in the parent process after the fork function call will only affect the parent process 's copy of x, and will not affect the child process' s copy of x.
