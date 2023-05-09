592ae46964240d2a7ee2e72578b307d48a71f748
Sy Rashid <sy.rashid.11@gmail.com>
2023-05-09 11:21:07 +0100

    FEAT: Finalize clock code for hw

--- vm-intro/memory-user.c
+++ vm-intro/memory-user.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(int argc, char *argv[])
 {
   int size = atoi(argv[1]);
-  int *arr = malloc(size * 1048576);
+  int time_in_seconds = atoi(argv[2]);
+  int *arr = calloc(size * 1048576, sizeof(int));
+  size_t arr_size = size * 1048576 * sizeof(int);
+
+  clock_t start_time = clock();
+
+  while ((clock() - start_time) / CLOCKS_PER_SEC < time_in_seconds)
+  {
+    for (int i = 0; i < arr_size; i++)
+    {
+      int current = arr[i];
+    }
+  }
+
+  free(arr);
 
   return 0;
 }
