bfd8f9172659b8017521443f68711a04dabadd42
Sy Rashid <sy.rashid.11@gmail.com>
2023-05-09 11:30:48 +0100

    CONF: Compile code

--- vm-intro/memory-user.c
+++ vm-intro/memory-user.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
+
+  if (argc != 3)
+  {
+    fprintf(stderr, "usage: memory-user <memory> <time>\n");
+    exit(EXIT_FAILURE);
+  }
+
   int size = atoi(argv[1]);
   int time_in_seconds = atoi(argv[2]);
   int *arr = calloc(size * 1048576, sizeof(int));
